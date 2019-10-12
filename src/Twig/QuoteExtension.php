a66463a97f3a4bf65f5cae30ed1d6cfb32fb7ff6
Ryan Linnit <ryan@ryanl.co.uk>
2019-10-12 21:50:05 +0100

    twig quote extension

---
+++ src/Twig/QuoteExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class QuoteExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('gt2quote', [$this, 'formatQuote'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function formatQuote($string)
+    {
+        return preg_replace('/(^&gt;.+$)/m', "<p class='text-dark'>$1</p>", $string);
+    }
+}
