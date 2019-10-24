8d6f3957536a9eb9b1bf619e7b35a3d93522a44f
Ryan Linnit <ryan@ryanl.co.uk>
2019-10-24 15:19:45 +0000

    Seperate routes for admin functions

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -16,41 +16,38 @@ use App\Repository\WordFilterRepository;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_index")
      */
-    public function index(BannedRepository $bannedRepository, WordFilterRepository $wordFilterRepository, Request $request)
+    public function index(): Response
     {
-        $bannedItems = $this->getBannedItems($bannedRepository, $request);
-        $bannedIPs = $bannedItems[0];
-        $bannedForm = $bannedItems[1];
-
-        $wordFilterItems = $this->getFilterItems($wordFilterRepository, $request);
-        $wordFilterStrings = $wordFilterItems[0];
-        $wordFilterForm = $wordFilterItems[1];
-
-        return $this->render('admin/index.html.twig', [
-            'banned_ips' => $bannedIPs,
-            'banned_form' => $bannedForm->createView(),
-            'word_filter_strings' => $wordFilterStrings,
-            'word_filter_form' => $wordFilterForm->createView()
-        ]);
+        return $this->render('admin/index.html.twig');
     }
 
-    public function getFilterItems(WordFilterRepository $wordFilterRepository, Request $request): Array
+    /**
+     * @Route("/admin/wordfilter", name="admin_wordfilter")
+     */
+    public function wordFilter(WordFilterRepository $wordFilterRepository, Request $request): Response
     {
         $wordFilter = new WordFilter();
         $wordFilterForm = $this->createForm(WordFilterType::class, $wordFilter);
         $wordFilterForm->handleRequest($request);
+
         $wordFilterStrings = $wordFilterRepository->findAll();
 
         if ($wordFilterForm->isSubmitted() && $wordFilterForm->isValid()) {
             $this->addBadWord($wordFilter);
         }
 
-        return [$wordFilterStrings, $wordFilterForm];
+        return $this->render('admin/wordfilter.html.twig', [
+            'word_filter_strings' => $wordFilterStrings,
+            'word_filter_form' => $wordFilterForm->createView()
+        ]);
     }
 
-    public function getBannedItems(BannedRepository $bannedRepository, Request $request): Array
+    /**
+     * @Route("/admin/bans", name="admin_banned")
+     */
+    public function banned(BannedRepository $bannedRepository, Request $request): Response
     {
         $banned = new Banned();
         $bannedForm = $this->createForm(BannedType::class, $banned);
@@ -61,7 +58,10 @@ class AdminController extends AbstractController
             $this->banIP($banned);
         }
 
-        return [$bannedIPs, $bannedForm];
+        return $this->render('admin/banned.html.twig', [
+            'banned_ips' => $bannedIPs,
+            'banned_form' => $bannedForm->createView()
+        ]);
     }
 
     /**
