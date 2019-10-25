7e37e54c42a661466644309cac4910921d3cf50a
Ryan Linnit <ryan@ryanl.co.uk>
2019-10-25 10:58:00 +0000

    smol

--- public/js/reply.js
+++ public/js/reply.js
@@ -4,10 +4,6 @@ function getQuoteText() {
         return false;
     }
 
-    // if (isValidQuote() === false) {
-    //     return false;
-    // }
-
     var quote = "";
     
     if (window.getSelection) {
@@ -19,28 +15,7 @@ function getQuoteText() {
     return quote;
 }
 
-function isValidQuote() {
-    var isSameNode = window.getSelection().anchorNode.isSameNode(window.getSelection().focusNode);
-
-    if (isSameNode === false) {
-        console.log("not same node", window.getSelection())
-        return false;
-    }
-
-    // Possibly partial message
-    if (typeof window.getSelection().focusNode.classList === "undefined") {
-        // Check if parent is a message <p>
-        if (window.getSelection().focusNode.parentElement.classList.contains('message') === false) {
-            console.log("parent not message", window.getSelection())
-            return false;
-        }
-    } else if (window.getSelection().focusNode.classList.contains('message') === false) {
-        console.log("full not message", window.getSelection());
-        return false;
-    }
-
-    return true;
-}
+var parentId;
 
 $(".reply_button").click(function(event) {
     event.preventDefault();
@@ -48,7 +23,6 @@ $(".reply_button").click(function(event) {
     var id;
     var quote;
     var messageElement = $("#post_message");
-    var oldId;
 
     this.classList.forEach(function(a_class) {
         // console.log(a_class);
@@ -56,11 +30,12 @@ $(".reply_button").click(function(event) {
         if (id) {
             id = id[1]
             console.log(id);
+            parentId = id;
+
             // Set parent post id on reply form
             // todo
-            oldId = $("#post_parent_post")[0].selectedIndex;
             
-            $(`#post_parent_post option[value=${oldId}]`).attr('selected', false);
+            $(`#post_parent_post option[value=${parentId}]`).attr('selected', false);
             $(`#post_parent_post option[value=${id}]`).attr('selected', true);
 
             // Retrieve any quote text and place in reply form message textarea
