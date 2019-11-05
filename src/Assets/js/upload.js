c4527e2bc154222ebbbd34ef150bf958978aa2ff
Ryan Linnit <ryan@ryanl.co.uk>
2019-11-05 13:40:51 +0000

    No jQuery required

--- src/Assets/js/upload.js
+++ src/Assets/js/upload.js
@@ -1,12 +1,22 @@
-$('#post_imageFile_file').on('change', function () {
-    let fullPath = $('#post_imageFile_file').val();
+let imageFile = document.getElementById("post_imageFile_file");
+
+imageFile.onchange = updateImageFileText;
+
+Array.from(document.getElementsByClassName("delete_button")).forEach(function(item) {
+    item.onclick = submitDeleteForm;
+});
+
+function updateImageFileText() {
+    let fullPath = imageFile.value;
     let fileName = fullPath.split(/(\\|\/)/g).pop();
 
-    $('.custom-file-label').text(fileName);
+    Array.from(document.getElementsByClassName("custom-file-label")).forEach(function(item) {
+        item.textContent = fileName;
+    });
 
-    $('#post_message').removeAttr('required');
-})
+    document.getElementById("post_message").removeAttribute("required"); 
+}
 
-$(".delete_button").click(function(event) {
+function submitDeleteForm() {
     this.parentElement.submit();
-});
+}
