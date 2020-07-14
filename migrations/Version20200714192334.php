686b199c618e8e5fdffcdc815b9de9893742a7a7
Ryan Linnit <ryan@ryanl.co.uk>
2020-07-14 20:24:41 +0100

    Consolidating migrations

--- migrations/Version20200714192334.php
+++ migrations/Version20200714192334.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191014174216 extends AbstractMigration
+final class Version20200714192334 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,11 +20,11 @@ final class Version20191014174216 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_880E0D76F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE board (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(127) NOT NULL, UNIQUE INDEX UNIQ_58562B475E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, board_id INT NOT NULL, parent_id INT DEFAULT NULL, title VARCHAR(127) DEFAULT NULL, message LONGTEXT NOT NULL, created DATETIME NOT NULL, latestpost DATETIME DEFAULT NULL, INDEX IDX_5A8A6C8DE7EC5785 (board_id), INDEX IDX_5A8A6C8D727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE banned (id INT AUTO_INCREMENT NOT NULL, ip_address VARCHAR(64) NOT NULL, ban_time DATETIME NOT NULL, unban_time DATETIME NOT NULL, reason VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE board (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(127) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_58562B475E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, board_id INT NOT NULL, parent_id INT DEFAULT NULL, title VARCHAR(127) DEFAULT NULL, message LONGTEXT DEFAULT NULL, created DATETIME NOT NULL, latestpost DATETIME DEFAULT NULL, image_name VARCHAR(127) DEFAULT NULL, ip_address VARCHAR(64) NOT NULL, INDEX IDX_5A8A6C8DE7EC5785 (board_id), INDEX IDX_5A8A6C8D727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE word_filter (id INT AUTO_INCREMENT NOT NULL, bad_word VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DE7EC5785 FOREIGN KEY (board_id) REFERENCES board (id)');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D727ACA70 FOREIGN KEY (parent_id) REFERENCES post (id) ON DELETE CASCADE');
     }
@@ -32,12 +32,12 @@ final class Version20191014174216 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8DE7EC5785');
         $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D727ACA70');
         $this->addSql('DROP TABLE admin');
+        $this->addSql('DROP TABLE banned');
         $this->addSql('DROP TABLE board');
         $this->addSql('DROP TABLE post');
+        $this->addSql('DROP TABLE word_filter');
     }
 }
