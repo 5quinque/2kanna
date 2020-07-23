dbc6ebbeb7bb79853fc08fb34799fe772ad62175
Ryan Linnit <ryan@ryanl.co.uk>
2020-07-23 11:36:17 +0100

    Made CrossLinkExtension easier to read

--- src/Twig/CrossLinkExtension.php
+++ src/Twig/CrossLinkExtension.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace App\Twig;
 
-use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
 use App\Repository\BoardRepository;
 use App\Repository\PostRepository;
 use App\Service\CrossLink;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 
 class CrossLinkExtension extends AbstractExtension
 {
@@ -19,6 +20,7 @@ class CrossLinkExtension extends AbstractExtension
         $this->postRepository = $postRepository;
         $this->crossLink = $crossLink;
     }
+
     public function getFilters()
     {
         return [
@@ -26,38 +28,56 @@ class CrossLinkExtension extends AbstractExtension
         ];
     }
 
-    // [TODO] tidy this
     public function createCrossLinks(string $string)
     {
         $regex = '/&gt;&gt;&gt;\/(\w+)\/?(\d+)?/';
 
         preg_match_all($regex, $string, $matches);
 
-        if ($matches[0]) {
-            for ($i = 0; $i < count($matches[0]); $i++) {
-                $text = $matches[0][$i];
-                $board = $matches[1][$i];
-                $post = $matches[2][$i];
+        for ($i = 0; $i < count($matches[0]); ++$i) {
+            $text = $matches[0][$i];
+            $board = $matches[1][$i];
+            $post = $matches[2][$i];
 
-                // Check if post is there, and if it's valid
-                if ($post) {
-                    $postEntity = $this->postRepository->findOneBy(["id" => $post]);
-                }
+            if (!$this->boardEntity($board)) {
+                continue;
+            }
 
-                // Check if the board is valid
-                $boardEntity = $this->boardRepository->findOneBy(["name" => $board]);
-                if ($boardEntity) {
-                    if (isset($postEntity)) {
-                        $url = $this->crossLink->generatePostUrl($boardEntity->getName(), $postEntity->getId());
-                    } else {
-                        $url = $this->crossLink->generateBoardUrl($boardEntity->getName());
-                    }
-
-                    $string = preg_replace("!$text!", "<a href='$url'>$text</a>", $string);
-                }
+            if ($this->postEntity($post)) {
+                $string = $this->replacePostUrl($text, $string, $board, $post);
+            } else {
+                $string = $this->replaceBoardUrl($text, $string, $board);
             }
         }
 
         return $string;
+    }
+
+    private function postEntity($post)
+    {
+        return $this->postRepository->findOneBy(['id' => $post]);
+    }
+
+    private function boardEntity($board)
+    {
+        return $this->boardRepository->findOneBy(['name' => $board]);
+    }
+
+    private function replacePostUrl($text, $string, $board, $post)
+    {
+        return preg_replace(
+            "!{$text}!",
+            "<a href='{$this->crossLink->generatePostUrl($board, $post)}'>{$text}</a>",
+            $string
+        );
+    }
+
+    private function replaceBoardUrl($text, $string, $board)
+    {
+        return preg_replace(
+            "!{$text}!",
+            "<a href='{$this->crossLink->generateBoardUrl($board)}'>{$text}</a>",
+            $string
+        );
     }
 }
