b48e740fb82812f3e4fa0d328fa19e0fd663957b
Ryan Linnit <ryan@ryanl.co.uk>
2020-07-27 23:10:03 +0100

    Cache settings

--- src/Util/SettingUtil.php
+++ src/Util/SettingUtil.php
@@ -3,27 +3,45 @@
 namespace App\Util;
 
 use App\Repository\SettingRepository;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class SettingUtil
 {
     private $settingRepository;
+    private $cache;
+    private $name;
 
     public function __construct(SettingRepository $settingRepository)
     {
         $this->settingRepository = $settingRepository;
+        $this->cache = new FilesystemAdapter();
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
     }
 
     public function setting(string $name)
     {
-        $result = $this->settingRepository->findOneBy(['name' => $name]);
-        if ($result) {
-            if ('checkbox' === $result->getType()) {
-                return $result->getValueBool();
+        $this->setName($name);
+
+        return $this->cache->get($name, function (ItemInterface $item) {
+            $item->expiresAfter(3600);
+
+            $result = $this->settingRepository->findOneBy(['name' => $this->name]);
+            if ($result) {
+                if ('checkbox' === $result->getType()) {
+                    return $result->getValueBool();
+                }
+
+                return $result->getValue();
             }
 
-            return $result->getValue();
-        }
-
-        return null;
+            return null;
+        });
     }
 }
