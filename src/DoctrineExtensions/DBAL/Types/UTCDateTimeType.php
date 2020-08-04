4fc652731fddb89c45639e313fbab522509344cb
Ryan Linnit <ryan@ryanl.co.uk>
2020-08-04 22:36:25 +0100

    Always store Datetime as UTC in database

---
+++ src/DoctrineExtensions/DBAL/Types/UTCDateTimeType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DoctrineExtensions\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType;
+
+class UTCDateTimeType extends DateTimeType
+{
+    private static $utc;
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof \DateTime) {
+            $value->setTimezone(self::getUtc());
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value || $value instanceof \DateTime) {
+            return $value;
+        }
+
+        $converted = \DateTime::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            self::getUtc()
+        );
+
+        if (!$converted) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeFormatString()
+            );
+        }
+
+        return $converted;
+    }
+
+    private static function getUtc()
+    {
+        return self::$utc ? self::$utc : self::$utc = new \DateTimeZone('UTC');
+    }
+}
