8265c1c8dce239f495c5507b487700a0f2891c5c
Ryan Linnit <ryan@ryanl.co.uk>
2020-08-17 22:12:50 +0100

    board javascript

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,4 +1,9 @@
 import '../css/prism.css';
 
+import * as Collapse from './collapse';
+import * as Image from './image';
 import './upload';
 import './prism';
+
+Collapse.setCollapseOnclick();
+Image.setImageOnclick();
